165782f4704bea79272b499a5cc737dce423a1b9
Jason <134750994+1WRP-MechaMaster@users.noreply.github.com>
2025-05-15 02:23:52 -0300

    Update fxmanifest.lua

--- TakeHostage/fxmanifest.lua
+++ TakeHostage/fxmanifest.lua
@@ -1,10 +1,26 @@
--- Resource Metadata
-fx_version 'bodacious'
-games { 'gta5' }
+fx_version 'cerulean'
+game 'gta5'
 
-author 'rubbertoe98'
-description 'TakeHostage'
-version '1.0.0'
+author 'Original: Robbster'
+description 'Take Hostage Script with ox_target integration'
+version '2.0.0'
 
-client_script "cl_takehostage.lua"
-server_script "sv_takehostage.lua"
+-- Define shared scripts
+shared_scripts {
+    'config.lua'
+}
+
+-- Define client scripts
+client_scripts {
+    'client/cl_takehostage.lua'
+}
+
+-- Define server scripts
+server_scripts {
+    'server/sv_takehostage.lua'
+}
+
+-- Define dependencies
+dependencies {
+    'ox_target'  -- Only required if Config.UseOxTarget is true
+}
